3574766e88e3c01243aa17759eae2f566e4327d6
anweshasur20 <149654750+anweshasur20@users.noreply.github.com>
2024-07-25 23:46:55 +0530

    Create Simple Calculator.py

---
+++ Simple Calculator.py
@@ -0,0 +1,17 @@
+print("```Simple Calculator```")
+num1=float(input("Enter the first number here: "))
+num2=float(input("Enter the second number here: "))
+
+print("Press 1 for addition\n Press 2 for subtraction\n Press 3 for multiplication\n Press 4 for division")
+choice=int(input("Enter your choice from 1-4: "))
+
+if choice==1:
+    print("The addition is", num1+num2)
+elif choice==2:
+    print("The subtraction is", num1-num2)
+elif choice==3:
+    print("The multiplication is", num1*num2)
+elif choice==4:
+    print("The division is", num1/num2)
+else:
+    print("Invalid Input")
